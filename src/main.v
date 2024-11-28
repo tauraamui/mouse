92302ff124349d3cb1c3eab2cae3ef2eda1c9b6c
tauraamui <adampstringer@protonmail.com>
2024-11-28 19:42:04 +0000

    feat: refresh update line

--- src/main.v
+++ src/main.v
@@ -2,8 +2,27 @@ module main
 
 import lib.display
 import lib.mouse
+import time
+
+struct Point {
+	x int
+	y int
+}
 
 fn main() {
 	x, y := display.get_pixel_width_height()
 	mouse.move_to(x / 2, y / 2)
+
+	points := [Point{ x: 720, y: 300 }, Point{ x: 820, y: 600 }, Point{ x: 520, y: 450 }, Point{ x: 920, y: 450 }, Point{ x: 620, y: 600 }, Point{ x: 720, y: 300 }]
+
+	for {
+		print("\r")
+		print("moving mouse points @ ${time.now()}")
+		flush_stdout()
+		for p in points {
+			mouse.move_to(p.x, p.y)
+			time.sleep(1 * time.second)
+		}
+		time.sleep(30 * time.second)
+	}
 }
